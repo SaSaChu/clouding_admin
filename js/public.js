d8f78b3533ee54a1ab7db95bc9b7e2c12649b8ef
OA Wu <comdan66@gmail.com>
2021-10-01 12:31:32 +0800

    Fix Code.

--- js/public.js
+++ js/public.js
@@ -1,5 +1,23 @@
+
 $(function() {
+  $('.cd-accordion-menu').each(function() {
+    let $that = $(this)
+    $that.find('> label').click(function() {
+      $(this).toggleClass('active')
+        .closest('.col')
+        .siblings()
+        .find('.cd-accordion-menu > label')
+        .removeClass('active')
+    })
+    $that.find('> div label').click(function() {
+      let $this = $(this)
+      if ($this.hasClass('active'))
+        return $this.removeClass('active')
+      else
+        $this.addClass('active')
+          .siblings()
+          .removeClass('active')
+    })
 
-
-
+  })
 });
